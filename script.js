f4f2df46f18e65d9efd6ccce5c51a3f70250da5e
Matthew Wiser <matt.wiser@icloud.com>
2022-07-10 19:48:55 -0500

    added color coding functionality

--- script.js
+++ script.js
@@ -2,8 +2,26 @@ var currentDate = moment().format("dddd MMMM Do");
 
 document.getElementById("currentDay").textContent = currentDate;
 
-var currentHour = moment().hour();
-var hourbefore = moment().isBefore(currentHour)
-var hourafter = moment().isAfter(currentHour)
+const currentHour = moment().hour();
+let textAreas = document.getElementsByTagName("textarea");
+// convert HTML collection of textAreas into an array
+textAreas = [].slice.call(textAreas);
 
-console.log(hourbefore, hourafter)
+function assessHour() {
+    textAreas.forEach(element => {
+        console.log(element.dataset.hour);
+        if (element.dataset.hour < currentHour) {
+            element.classList.add("past");
+        }
+        else if (element.dataset.hour > currentHour) {
+            element.classList.add("future");
+        }
+        else {
+            element.classList.add("present");
+        }
+
+    });
+}
+
+console.log(currentHour);
+assessHour();
